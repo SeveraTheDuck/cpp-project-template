5830a9a1c82e09e07afb5650b7960d0f86800a47
SeveraTheDuck <severa.dev@gmail.com>
2026-03-28 04:40:49 +0300

    Matrix ci/cd

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "C++23 Template Environment";
+  description = "C++23 Template Environment (Linux Only)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -11,22 +11,31 @@
       let
         pkgs = import nixpkgs { inherit system; };
         llvm = pkgs.llvmPackages_latest;
+        gcc = pkgs.gcc14;
       in
       {
-        devShells.default = pkgs.mkShell.override { stdenv = llvm.stdenv; } {
+        devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             cmake
             ninja
             ccache
 
-            llvm.clang-tools          # clang-format, clang-tidy, clangd
-            cmake-format              # cmake-format
-            editorconfig-checker      # Validate .editorconfig
-            pre-commit                # git-hooks framework
+            llvm.clang        # clang, clang++
+            gcc               # gcc, g++
+            
+            llvm.clang-tools  # clang-format, clang-tidy, clangd
+            cmake-format
+            editorconfig-checker
+            pre-commit
+
+            gtest
           ];
 
           shellHook = ''
-            echo "🚀 C++23 Env Loaded: Clang $(clang++ -dumpversion), CMake $(cmake --version | head -n 1 | awk '{print $3}')"
+            echo "C++23 Environment Loaded"
+            echo "Clang: $(clang++ -dumpversion)"
+            echo "GCC:   $(g++ -dumpversion)"
+            echo "CMake: $(cmake --version | head -n 1 | awk '{print $3}')"
 
             if [ -d .git ]; then
               pre-commit install --hook-type pre-push > /dev/null
